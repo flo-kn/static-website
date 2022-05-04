3829c21176bbcca10f507e169a7fd6467e6c42d4
flo-kni <florian@knip-builds.de>
2022-05-04 15:05:03 +0200

    some documentation

--- index.ts
+++ index.ts
@@ -4,13 +4,13 @@ import * as mime from "mime";
 
 
 // Create an S3 bucket
-let siteBucket = new aws.s3.Bucket("s3-website-bucket", {
+const siteBucket = new aws.s3.Bucket("s3-website-bucket", {
     website: {
       indexDocument: "index.html",
     },
   });
 
-let siteDir = "www"; // directory for content files
+const siteDir = "www"; // directory for content files
 
 // For each file in the directory, create an S3 object stored in `siteBucket`
 for (let item of require("fs").readdirSync(siteDir)) {
@@ -27,3 +27,31 @@ for (let item of require("fs").readdirSync(siteDir)) {
 
 exports.bucketName = siteBucket.bucket; // create a stack export for bucket name
 
+const bucketName = siteBucket.bucketDomainName
+
+// Create an S3 Bucket Policy to allow public read of all objects in bucket
+// This reusable function can be pulled out into its own module
+function publicReadPolicyForBucket(bucketName: string) {
+    return JSON.stringify({
+      Version: "2012-10-17",
+      Statement: [{
+        Effect: "Allow",
+        Principal: "*",
+        Action: [
+          "s3:GetObject"
+        ],
+        Resource: [
+          `arn:aws:s3:::${bucketName}/*` // policy refers to bucket name explicitly
+        ]
+      }]
+    })
+  }
+  
+  // Set the access policy for the bucket so all objects are readable
+  let bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
+    bucket: siteBucket.bucket, // depends on siteBucket -- see explanation below
+    policy: siteBucket.bucket.apply(publicReadPolicyForBucket)
+            // transform the siteBucket.bucket output property -- see explanation below
+  });
+  
+  exports.websiteUrl = siteBucket.websiteEndpoint; // output the endpoint as a stack output
